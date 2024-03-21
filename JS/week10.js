ca4c70519a069d994024d07dbb0fe686284e77d8
ej-133 <elre3593@colorado.edu>
2024-03-20 21:30:32 -0600

    yessir

---
+++ JS/week10.js
@@ -0,0 +1,18 @@
+<script>
+const button = document.querySelector("button");
+button.addEventListener('click', changeText);
+
+const heading= document.querySelector("h1");
+
+function changeText (){
+    //alert(`text successful`);
+    const head = prompt ("tell me a scaret");
+    heading.textContent = `boooo`:
+
+    //console.log("test");
+    //function runfunction (){
+
+}
+
+
+</script>
