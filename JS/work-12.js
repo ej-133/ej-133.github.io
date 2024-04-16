43bf6a097fe3b61397c9d66d04caa3f56488c2aa
ej-133 <elre3593@colorado.edu>
2024-04-16 01:35:27 -0600

    yuo

---
+++ JS/work-12.js
@@ -0,0 +1,22 @@
+const apiUrl = 'https://poetrydb.org/title/Ozymandias/lines.json';
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Data not found');
+      } else if (response.status === 500) {
+        throw new Error('Server error');
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    }
+    return response.json();
+  })
+  .then(data => {
+    outputElement.textContent = JSON.stringify(data, null, 2);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
